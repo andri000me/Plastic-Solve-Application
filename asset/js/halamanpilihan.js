92c44a632b68d08f468b071a481c16c47f5fb62a
Kristianti Yulistia <yulistiakristianti1@gmail.com>
2020-03-15 00:29:49 -0700

    sudah fiks

--- asset/js/halamanpilihan.js
+++ asset/js/halamanpilihan.js
@@ -4,6 +4,6 @@ var hargaTotebag = localStorage.getItem('hargaTotebag');
 var alamatTotebag = localStorage.getItem('alamatTotebag');
 
 $('#username').html(username);
-$('#ukuranTotebag').html(ukuranTotebag);
-$('#hargaTotebag').html('Rp. ' + hargaTotebag)
-$('#alamatTotebag').html(alamatTotebag)
+$('#ukuranTotebag').html(': ' + ukuranTotebag);
+$('#hargaTotebag').html(': ' + 'Rp. ' + hargaTotebag)
+$('#alamatTotebag').html(': ' + alamatTotebag)
